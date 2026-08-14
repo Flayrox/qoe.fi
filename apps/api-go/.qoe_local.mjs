e73f56432cb28a7548156ccd3a712ebeca45a4a6
Flayrox <epheofc@gmail.com>
2026-08-14 17:19:17 +0200

    chore(api-go): remove stray temp test scripts + ignore *.mjs/*.js

--- apps/api-go/.qoe_local.mjs
+++
@@ -1,5 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-const p = new PrismaClient();
-const u = await p.user.findFirst({ select: { id: true, username: true } });
-console.log(u ? `${u.id}|${u.username}` : '');
-await p.$disconnect();
